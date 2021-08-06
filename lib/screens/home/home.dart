10051eef379d249ec6d43ffcb0b0e40a4ff879c4
Steve Narancic <58119793+stevenarancic@users.noreply.github.com>
2021-08-05 21:30:57 -0300

    trip_advisor
    novidades:
    - inserção da imagem de banner;
    - inserção dos assets no pubspec.yaml;
    - biblioteca font_awesome_icons adicionada ao projeto.

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: Image.asset(
+                "assets/images/banner_image.jpg",
+              ),
+            ),
+            Row(
+              children: [
+                Text(
+                  "data",
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
