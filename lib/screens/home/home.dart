1e5c148c83c3d884993b24eb139c7cfb183379af
Steve Narancic <58119793+stevenarancic@users.noreply.github.com>
2021-08-08 21:52:51 -0300

    Update home.dart

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -38,8 +38,11 @@ class Home extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              Text(
-                "É impossível não se render aos encantos de Paris, a bela capital francesa e destino turístico frequentado por milhões de pessoas todos os anos. Vibrante, charmosa, romântica, divertida, além de berço da cultura e da arte, a Cidade Luz, como é chamada, possui uma infindável lista de qualidades.",
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Text(
+                  "É impossível não se render aos encantos de Paris, a bela capital francesa e destino turístico frequentado por milhões de pessoas todos os anos. Vibrante, charmosa, romântica, divertida, além de berço da cultura e da arte, a Cidade Luz, como é chamada, possui uma infindável lista de qualidades.",
+                ),
               ),
               Text(
                 "Fotos",
